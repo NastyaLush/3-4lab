d72b61f6b49654f6a026a5c4ad0b6056763ad9ab
bhmaa <ddz.sm@yandex.ru>
2022-01-16 17:50:36 +0300

    --

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,2 @@
-import factory.CasioFactory;
-import peoples.KikuoIbe;
-
 public class Main {
-    public static void main(String [] args){
-        CasioFactory casioFactory = new CasioFactory();
-        KikuoIbe kikuoIbe = KikuoIbe.getInstance();
-        kikuoIbe.getAJobICasioFactory(casioFactory);
-        System.out.println(casioFactory.getNumberOfEmployees());
-        casioFactory.deleteEmployee(kikuoIbe);
-        System.out.println(casioFactory.getNumberOfEmployees());
-        casioFactory.deleteEmployee(kikuoIbe);
-        System.out.println(casioFactory.getNumberOfEmployees());
-    }
 }
